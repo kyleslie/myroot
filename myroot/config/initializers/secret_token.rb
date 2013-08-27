35d24858ebef54064f494539250e8802966bbb80
Kyle Leslie Sim <kyle.l.sim@gmail.com>
2013-08-27 02:31:54 +0100

    Initial Commit

---
+++ myroot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myroot::Application.config.secret_token = '0e8d3924b608e0a650c3211f0fce69438a7185b93ab134124ddca1db0c659444df81d54330e91643ba8012bb888fde54e820731008192fb42b8e17aa4ecf1f19'
